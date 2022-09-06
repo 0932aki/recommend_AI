79d7c4b2e44f37ea2c1d0ef8f0c88a499250a101
0932aki <ogumitsu11@gmail.com>
2022-09-06 12:25:42 +0900

    18

--- recommend_AI.py
+++ recommend_AI.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("興味深掘り支援AI")
 st.subheader("あなたが興味あることを入力してください")
+st.subheader("広がり支援")
 #st.subheader("例：私は釣りと[MASK]が好きです")
 message = st.text_input("例：釣り")
 
@@ -17,6 +18,64 @@ def recommend_AI(message):
     model = RobertaForMaskedLM.from_pretrained("rinna/japanese-roberta-base")
     st.subheader("AIがあなたにおすすめする内容")
     message = '私は'+ message + 'と[MASK]が好きです'
+
+    # original text
+    text_orig = message
+
+    # prepend [CLS]
+    text = "[CLS]" + text_orig
+
+    # tokenize
+    tokens = tokenizer.tokenize(text)
+    #print(tokens)
+
+    #print('mask index :' , tokens.index('[MASK]'))
+    masked_idx = tokens.index('[MASK]')
+
+    tokens[masked_idx] = tokenizer.mask_token
+
+    # convert to ids
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # convert to tensor
+    import torch
+    token_tensor = torch.tensor([token_ids])
+
+    # get the top 10 predictions of the masked token
+    model = model.eval()
+    with torch.no_grad():
+        outputs = model(token_tensor)
+        predictions = outputs[0][0, masked_idx].topk(10)
+
+    #print(text_orig)
+
+    for i, index_t in enumerate(predictions.indices):
+        index = index_t.item()
+        token = tokenizer.convert_ids_to_tokens([index])[0]
+        if token not in  ['セックス']:
+            st.write(i+1, token)
+
+if st.button("送信"):
+    recommend_AI(message)
+
+
+import streamlit as st
+
+st.subheader("深まり支援")
+message = st.text_input("例：釣り")
+
+
+
+def recommend_AI(message):
+    from transformers import T5Tokenizer, RobertaForMaskedLM
+    
+
+    tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-roberta-base")
+    tokenizer.do_lower_case = True  # due to some bug of tokenizer config loading
+
+    model = RobertaForMaskedLM.from_pretrained("rinna/japanese-roberta-base")
+    st.subheader("AIがあなたにおすすめする内容")
+    message = '私は'+ message + 'の中でも特に[MASK]が好きです'
 
     # original text
     text_orig = message
