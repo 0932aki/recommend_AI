4f4f921582e854666477616abe4e5f204f4de87a
0932aki <ogumitsu11@gmail.com>
2022-12-15 16:04:27 +0900

    b

--- recommend_AI.py
+++ recommend_AI.py
@@ -115,3 +115,59 @@ def recommend_AI2(message2):
 
 if st.button("深まり依頼"):
     recommend_AI2(message)
+
+
+keyword1 = st.text_input("キーワード1")
+keyword2 = st.text_input("キーワード2")
+
+
+
+def recommend_AI3(keyword1,keyword2):
+    from transformers import T5Tokenizer, RobertaForMaskedLM
+    
+
+    tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-roberta-base")
+    tokenizer.do_lower_case = True  # due to some bug of tokenizer config loading
+
+    model = RobertaForMaskedLM.from_pretrained("rinna/japanese-roberta-base")
+    st.subheader("AIがあなたにおすすめする内容")
+    message12 = '私は'+ keyword1 + 'の' + keyword2 + 'の[MASK]についてよく知っています'
+
+    # original text
+    text_orig = message12
+
+    # prepend [CLS]
+    text = "[CLS]" + text_orig
+
+    # tokenize
+    tokens = tokenizer.tokenize(text)
+    #print(tokens)
+
+    #print('mask index :' , tokens.index('[MASK]'))
+    masked_idx = tokens.index('[MASK]')
+
+    tokens[masked_idx] = tokenizer.mask_token
+
+    # convert to ids
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # convert to tensor
+    import torch
+    token_tensor = torch.tensor([token_ids])
+
+    # get the top 10 predictions of the masked token
+    model = model.eval()
+    with torch.no_grad():
+        outputs = model(token_tensor)
+        predictions = outputs[0][0, masked_idx].topk(10)
+
+    #print(text_orig)
+
+    for i, index_t in enumerate(predictions.indices):
+        index = index_t.item()
+        token = tokenizer.convert_ids_to_tokens([index])[0]
+        if token not in  ['セックス']:
+            st.write(i+1, token)
+
+if st.button("さらに深まり依頼"):
+    recommend_AI2(keyword1,keyword2)
